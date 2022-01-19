4bd22176c6c48e881c0d809d37946e5d3f5a0d6c
Ganning Xu <ganningxu@gmail.com>
2022-01-19 01:12:49 -0500

    finish blog

--- frontend/layouts/blog.js
+++ frontend/layouts/blog.js
@@ -1,15 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import Head from 'next/head'
+import React from 'react'
 import { Heading } from '../components/Text/Heading'
 
 export default function BlogLayout({ children, frontMatter }) {
     return (
         <>
-            <Heading>
-                {frontMatter.title}
-            </Heading>
+            <Head>
+                <title>{`${frontMatter.title} – RoboReach`}</title>
+            </Head>
 
             <div className="container mx-auto p-4">
-                {children}
+
+                <div className="mt-4 max-w-4xl mx-auto">
+                    <Heading>
+                        {frontMatter.title}
+                    </Heading>
+                    <img src={frontMatter.image} className='mx-auto  rounded-md' />
+
+                    <p className="font-semibold italic text-xl mt-4 text-center">{frontMatter.readingTime.text}</p>
+
+
+                    <hr className="my-4 max-w-4xl" />
+                    {children}
+
+                </div>
             </div>
         </>
     )
--- frontend/pages/impact.js
+++ frontend/pages/impact.js
@@ -14,7 +14,10 @@ import Team from '../data/team.json'
 import ByNumbers from '../data/byNumbers.json'
 import { TriColCounter } from '../components/Displays/TriColCounter'
 import { ChartDualCol } from '../components/Displays/ChartDualCol'
-export default function Impact() {
+import { getAllFilesFrontMatter } from '../lib/mdx'
+import { BlogCard } from '../components/Cards/BlogCard'
+export default function Impact({ posts }) {
+
     return (
         <>
             <Head>
@@ -34,9 +37,39 @@ export default function Impact() {
                 <ChartDualCol />
 
 
+                <MedSep />
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-4">
+                    {
+                        posts.map((post, index) => {
+                            return (
+                                <BlogCard
+                                    key={index}
+                                    title={post.title}
+                                    image={post.image}
+                                    description={post.summary}
+                                    publishedAt={post.publishedAt}
+                                    link={"/blog/" + post.slug}
+                                />
+                            )
+                        })
+                    }
+                </div>
+
                 <Footer />
             </div>
 
         </>
     )
+}
+
+export async function getStaticProps() {
+
+    const categories = ['blog'];
+    const finalPosts = [];
+
+    for (var i = 0; i < categories.length; i++) {
+        const retrievedPosts = await getAllFilesFrontMatter(categories[i])
+        finalPosts.push(...retrievedPosts)
+    }
+    return { props: { posts: finalPosts } }
 }
