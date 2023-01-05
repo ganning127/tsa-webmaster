9ae844028bc50944739eb42002373ebbce0fa3be
Ganning Xu <ganningxu@gmail.com>
2023-01-04 22:00:10 -0500

    Update AboutThisSite.jsx

--- frontend/components/OneCol/AboutThisSite.jsx
+++ frontend/components/OneCol/AboutThisSite.jsx
@@ -14,8 +14,6 @@ export const AboutThisSite = () => {
                 <Text fontSize='xl'>RoboReach's website was created by Ganning Xu, Jacob Van Meter, Daniel Zhang, Allison Zhang, and Max Caza for the 2022 TSA conference</Text>
 
                 <Box mt="4">
-                    <Text fontSize='lg'><chakra.span fontWeight='bold'>Plan of work</chakra.span>: <Link href='/plan-of-work.pdf' color='blue.light' isExternal>PDF Log</Link></Text>
-                    <Text fontSize='lg'><chakra.span fontWeight='bold'>Copyright Release Form</chakra.span>: <Link href='/copyright-checklist.pdf' color='blue.light' isExternal>PDF Form</Link></Text>
                     <Text fontSize='lg'><chakra.span fontWeight='bold'>GitHub Repository</chakra.span>: <Link href='https://github.com/ganning127/tsa-webmaster' color='blue.light' isExternal>Code Repo</Link></Text>
 
                 </Box>
